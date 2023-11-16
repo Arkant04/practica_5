a79fdbdd2873ad521bb04a77fb09dd62acc8e0b5
Arkant04 <delacorte19@gmail.com>
2023-11-16 12:55:10 +0100

    MOD se añade colores y texto

--- scripts/texto.js
+++ scripts/texto.js
@@ -2,4 +2,15 @@ function texInput(){
     const texto_usuario = document.querySelector("#input_text")
     console.log(texto_usuario.value)
     document.querySelector("#texto").innerHTML = texto_usuario.value
+    if(texto_usuario.value === ""){
+        document.querySelector("#texto").innerHTML = "Escibe de nuevo por fi​"
+    }
+    const R = Math.floor (Math.random() *255)
+    const G = Math.floor (Math.random() *255)
+    const B = Math.floor (Math.random() *255)
+    document.querySelector("#texto").style.color = `rgb(${R}, ${G}, ${B})`
+   
 }
+
+
+
